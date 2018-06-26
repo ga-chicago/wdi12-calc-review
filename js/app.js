0405425d511ec70412e172c96e8b318bae006ed0
Reuben Ayres <ra@sent.com>
2018-06-26 10:30:46 -0500

    can't type more than one .

--- js/app.js
+++ js/app.js
@@ -17,23 +17,38 @@ $('.num').on('click', (e) => {
   const $clickedDiv = $(e.currentTarget)
 
   // console.log($clickedDiv, "here's the same element, but jqueryified, we needed to this so we could use jquery methods on it")
-  console.log($clickedDiv)
+  // console.log($clickedDiv)
   
   // get the number (which is a string) from the div
   const theNum = $clickedDiv.text()
-  console.log(theNum)
-
-  // add number to display
-  // $('#display').append(theNum)
+  // console.log(theNum)
 
   // grab text from display
   const displayText = $('#display').text()
 
-  // add (concatenate) theNum to it
-  const newDisplayText = displayText + theNum;
+  // find out if there was a period
+  let periodFound = false;
+  if(theNum === ".") {
+    const displayArray = displayText.split('')
+    // look thru disp text for periods
+    for(let i = 0; i < displayArray.length; i++) {
+      if(displayArray[i] === ".") {
+        periodFound = true;
+      }
+    }        
+  }
 
-  // put the new concatenated value back in the display
-  $('#display').text(newDisplayText);
+  // if period
+  if(periodFound) {
+    // do nothing
+    
+  } else  {
+    
+    // add (concatenate) theNum to it
+    const newDisplayText = displayText + theNum;
+    // put the new concatenated value back in the display
+    $('#display').text(newDisplayText);
+  }
 
 })
 
